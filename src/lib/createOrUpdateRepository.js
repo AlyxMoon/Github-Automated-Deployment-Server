fc36801e16c2b40334466059b0c84ab730e54de7
Allister <allisterkmoon@gmail.com>
2019-03-28 20:14:29 -0700

    Fix check for whether repository exists

--- src/lib/createOrUpdateRepository.js
+++ src/lib/createOrUpdateRepository.js
@@ -9,10 +9,7 @@ const { WEB_DIRECTORY } = require('../../config').server
 // Then, will run a provided file in the repository (.alyxci) to take care of things such as restarting the app
 const createOrUpdateRepository = (repositoryInfo) => {
   fs.readdir(WEB_DIRECTORY)
-    .then(files => {
-      console.log(WEB_DIRECTORY, repositoryInfo.name, files)
-      return repositoryInfo.name in files ? updateRepository(repositoryInfo) : createRepository(repositoryInfo)
-    })
+    .then(files => files.includes[repositoryInfo.name] ? updateRepository(repositoryInfo) : createRepository(repositoryInfo))
     .then(() => runRepositoryExecFile(repositoryInfo) )
     .catch(error => console.error(`ERROR: ${error.message}`) )
 }
