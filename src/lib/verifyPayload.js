7570e07850308db16340711c227151fad709580d
Allister <allisterkmoon@gmail.com>
2019-04-01 13:26:53 -0700

    Update buffer methods

--- src/lib/verifyPayload.js
+++ src/lib/verifyPayload.js
@@ -2,7 +2,7 @@ const crypto = require('crypto')
 
 const verifyPayload = (payloadBody, payloadSignature) => {
   const signature = 'sha1=' + crypto.createHmac('sha1', process.env.SECRET_TOKEN).update(payloadBody).digest('hex')
-  return crypto.timingSafeEqual(new Buffer(signature), new Buffer(payloadSignature))
+  return crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(payloadSignature))
 }
 
 module.exports = verifyPayload
